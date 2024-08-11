fff265489e0674a37dac219abd6366509284dcd8
Hendrik <hendrik.reimers@gmail.com>
2024-08-11 20:55:48 +0200

    UI improvements (Copy to clipboard)

--- Resources/Public/Javascript/script.js
+++ Resources/Public/Javascript/script.js
@@ -1,16 +1,55 @@
-window.addEventListener("load", (evt) => {
+window.addEventListener("DOMContentLoaded", (evt) => {
     const tag = document.querySelector('script#script');
-    
+    const shortUrlInput = document.querySelector('input#shorturl');
+    let statusTimeout = null;
+
+
+    /**
+     * Helper function to show copy to clipboard status
+     *
+     * @param type
+     * @param msg
+     */
+    function setStatusDiv(type, msg) {
+        const statusDiv = document.querySelector('div.status');
+
+        if ( statusTimeout ) clearTimeout(statusTimeout);
+
+        if ( statusDiv ) {
+            statusDiv.classList.add(type);
+            statusDiv.innerText = msg;
+            statusTimeout = setTimeout(() => {
+                statusDiv.innerText = '';
+                statusDiv.classList.remove('error');
+                statusDiv.classList.remove('success');
+            }, 1000);
+        }
+    }
+
+    // URL rewriter (without browser history)
     if ( tag && tag.dataset.check ) {
         window.location.replace(atob(tag.dataset.check));
     }
-    
-    const shortUrlInput = document.querySelector('input#shorturl');
-    
+
+    // On Input focus
     if ( shortUrlInput ) {
         shortUrlInput.addEventListener('click', (evt) => {
-            evt.target.focus();
-            evt.target.select();
+            setTimeout(() => {
+                evt.target.focus();
+
+                setTimeout(() => {
+                    evt.target.setSelectionRange(0, evt.target.value.length);
+
+                    // Try to copy it into the clipboard
+                    navigator.clipboard.writeText(evt.target.value)
+                        .then(() => {
+                            setStatusDiv('success', 'Copied to clipboard.');
+                        })
+                        .catch(err => {
+                            setStatusDiv('error', 'Error copying to clipboard: ' + err.message);
+                        });
+                }, 50);
+            }, 50);
         });
     }
 });
